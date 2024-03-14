9aa9dc0e075f23a5e9757482d6cfdbaaf6de6cf4
ArmandoCivini <tcivini@gmail.com>
2024-03-13 23:29:45 -0300

    finished implementing aes_key, testing needed

--- src/aes_key/aes_key.jl
+++ src/aes_key/aes_key.jl
@@ -7,8 +7,14 @@ using Pkg
 Pkg.add("StaticArrays")
 using StaticArrays
 
-struct AESKey
-    data::SVector{N_B * (N_R + 1), Word}
+const AESKey = Vector{Word}
+
+const CipherKey = NTuple{4 * N_K, UInt8}
+
+
+function new_direct(cipher_key::CipherKey)
+    aes_key = zeros(Word, N_B * (N_R + 1))
+    return expand_key(cipher_key, aes_key)
 end
 
 function rot_word(word::Word)
@@ -22,4 +28,66 @@ function get_byte_from_word(word::Word, pos::UInt)
 
     return (word >> (8 * pos)) & 0xFF
 end
+
+function expand_key(cipher_key::CipherKey, aes_key::AESKey)
+    temp::Word = aes_key[N_K]
+    i::UInt8 = 1
+    while i <= N_K
+        aes_key[i] = big_endian_to_native(cipher_key[(4 * (i - 1) + 1):(4 * i)])#Word(cipher_key[(4 * (i - 1) + 1):(4 * i)] |> be32)
+        i += 1
+    end
+
+    i = N_K + 1
+
+    while i <= N_B * (N_R + 1)
+        temp = aes_key[i - 1]
+
+        if i % N_K == 1
+            temp = sub_word(rot_word(temp)) ⊻ R_CON[i ÷ N_K]
+        end
+
+        aes_key[i] = aes_key[i - N_K + 1] ⊻ temp
+        i += 1
+    end
+    return aes_key
+end
+
+function big_endian_to_native(endian_tuple::Tuple{UInt8,UInt8,UInt8,UInt8})::UInt32
+    # Unpack the tuple into individual bytes
+    byte1, byte2, byte3, byte4 = endian_tuple
+    
+    # Concatenate the bytes into a big-endian 32-bit integer
+    big_endian_uint32 = (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4
+    
+    # Convert the big-endian integer to the system's native endianness
+    return ntoh(big_endian_uint32)
+end
+
+function get_byte(uint32_value::UInt32, byte_index::UInt8)::UInt8
+    if byte_index < 0 || byte_index > 3
+        throw(ArgumentError("Byte index must be in the range 0-3"))
+    end
+    byte_shift = 8 * byte_index
+    byte_mask = UInt32(0xFF) << byte_shift
+    return (uint32_value & byte_mask) >> byte_shift
+end
+
+function apply_s_box(value::UInt8)::UInt8
+    pos_x = Int(value >> 4) + 1
+    pos_y = Int(value & 0x0F) + 1
+    S_BOX[pos_x, pos_y]
+end
+
+function sub_word(word::UInt32)::UInt32
+    result = 0
+
+    for i::UInt8 in 0:3
+        byte = get_byte(word, i)
+        new_byte = apply_s_box(byte)
+        result |= UInt32(new_byte) << (8 * i)
+    end
+    
+    return result
+end
+
 end # module aes_key
--- src/main.jl
+++ src/main.jl
@@ -4,6 +4,10 @@ using .constants
 include("aes_key/aes_key.jl")
 using .aes_key
 
+using Pkg
+Pkg.add("StaticArrays")
+using StaticArrays
+
 print(S_BOX, "\n")
 print(INV_S_BOX, "\n")
 print(R_CON, "\n")
@@ -11,4 +15,8 @@ word::Word = 0x12345678
 for pos::UInt in 0:3
     println("Byte at position ", pos, ": ", aes_key.get_byte_from_word(word, pos))
 end
+my_tuple::NTuple{16, UInt8} = (0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c)
+cipher_key = aes_key.CipherKey(my_tuple)
+key = aes_key.new_direct(cipher_key)
+print(key, "\n")
 print("hello world!\n")
