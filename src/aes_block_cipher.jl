ace18ebafda2359d6d749e90b46945bbe8d28c0b
ArmandoCivini <tcivini@gmail.com>
2024-03-21 16:37:08 -0300

    added AESBlockCipher constructor

--- src/aes_block_cipher.jl
+++ src/aes_block_cipher.jl
@@ -2,4 +2,16 @@ include("constants.jl")
 using .constants
 
 include("aes_key/aes_key.jl")
-using .aes_key
+using .new_direct
+
+struct AESBlockCipher
+    expanded_key::AESKey
+    inv_expanded_key::AESKey
+end
+
+function new(cipher_key::Vector{UInt8})
+    expanded_key = new_direct.new_direct(cipher_key)
+    inv_expanded_key = new_direct.new_inverse(cipher_key)
+
+    return AESBlockCipher(expanded_key, inv_expanded_key)
+end
