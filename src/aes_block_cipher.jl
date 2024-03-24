cdfecea607d5855e799402ab8b6302609e2f2c60
ArmandoCivini <tcivini@gmail.com>
2024-03-24 18:39:56 -0300

    added cipher block functions

--- src/aes_block_cipher.jl
+++ src/aes_block_cipher.jl
@@ -6,6 +6,9 @@ using .constants
 include("aes_key/aes_key.jl")
 using .aes_key
 
+include("state.jl")
+using .state
+
 struct AESBlockCipher
     expanded_key::AESKey
     inv_expanded_key::AESKey
@@ -16,6 +19,62 @@ function new(cipher_key::CipherKey)
     inv_expanded_key = aes_key.new_inverse(cipher_key)
 
     return AESBlockCipher(expanded_key, inv_expanded_key)
+end
+
+function to_be_bytes(cypher_key::UInt128)
+    cypher_key = hton(cypher_key)
+    return reinterpret(UInt8, [uint128_integer])
+end
+
+function new_u128(cypher_key::UInt128)
+    cypher_key = to_be_bytes(cypher_key)
+    return new(cypher_key)
+end
+
+function cipher_block(expanded_key::AESKey, data_in::Vector{UInt8})
+    data_out = zeros(UInt8, 4 * N_B)
+
+    _state::State = state.new_from_data_in(data_in)
+
+    state.add_round_key(_state, expanded_key[1:N_B])
+
+    for round in 1:N_R-1
+        state.sub_bytes(_state)
+        state.shift_rows(_state)
+        state.mix_columns(_state)
+        state.add_round_key(_state, expanded_key[(round*N_B)+1:((round+1)*N_B)])
+    end
+    state.sub_bytes(_state)
+    state.shift_rows(_state)
+    state.mix_columns(_state)
+    state.add_round_key(_state, expanded_key[(N_R*N_B)+1:((N_R+1)*N_B)])
+
+    state.set_data_out(data_out)
+
+    return data_out
+end
+
+function inv_cipher_block(expanded_key::AESKey, data_in::Vector{UInt8})
+    data_out = zeros(UInt8, 4 * N_B)
+
+    _state::State = state.new_from_data_in(data_in)
+
+    state.add_round_key(_state, inv_expanded_key[1:N_B])
+
+    for round in reverse(1:N_R-1)
+        state.sub_bytes(_state)
+        state.shift_rows(_state)
+        state.mix_columns(_state)
+        state.add_round_key(_state, inv_expanded_key[(round*N_B)+1:((round+1)*N_B)])
+    end
+    state.sub_bytes(_state)
+    state.shift_rows(_state)
+    state.mix_columns(_state)
+    state.add_round_key(_state, inv_expanded_key[1:N_B])
+
+    state.set_data_out(data_out)
+
+    return data_out
 end
 
 end
