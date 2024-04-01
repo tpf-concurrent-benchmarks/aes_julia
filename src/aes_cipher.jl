d7a6b081370b60063158411b267a882fbf09c81e
ArmandoCivini <tcivini@gmail.com>
2024-04-01 16:24:45 -0300

    fixed bug in cipher

--- src/aes_cipher.jl
+++ src/aes_cipher.jl
@@ -22,18 +22,18 @@ function cipher_blocks(blocks::Vector{Vector{UInt8}}, block_cipher::AESBlockCiph
     results
 end
 
-function cipher(input_path::String, output_path::String, cipher_key::CipherKey)
+function cipher(input_path::String, output_path::String, cipher_key::CipherKey, buffer_size::Int)
     block_cipher = aes_block_cipher.new(cipher_key)
     reader = chunk_reader.ChunkReader(input_path, 16, true)
     writer = chunk_writer.ChunkWriter(output_path, false)
-    buffer = [fill(UInt8(0), 16) for i in 1:2]
+    buffer = [Vector{UInt8}(undef, 16) for i in 1:buffer_size]
     while true
-        chunks_filled = chunk_reader.read_chunks(reader, 2, buffer)
+        chunks_filled = chunk_reader.read_chunks(reader, buffer_size, buffer)
         if chunks_filled == 1
             break
         end
 
-        results = cipher_blocks(buffer, block_cipher)
+        results = cipher_blocks(buffer[1:chunks_filled], block_cipher)
 
         chunk_writer.write_chunks(writer, results)
     end
@@ -49,18 +49,18 @@ function decipher_blocks(blocks::Vector{Vector{UInt8}}, block_cipher::AESBlockCi
     results
 end
 
-function decipher(input_path::String, output_path::String, cipher_key::CipherKey)
+function decipher(input_path::String, output_path::String, cipher_key::CipherKey, buffer_size::Int)
     block_cipher = aes_block_cipher.new(cipher_key)
     reader = chunk_reader.ChunkReader(input_path, 16, false)
     writer = chunk_writer.ChunkWriter(output_path, true)
-    buffer = [fill(UInt8(0), 16) for i in 1:2]
+    buffer = [Vector{UInt8}(undef, 16) for i in 1:buffer_size]
     while true
-        chunks_filled = chunk_reader.read_chunks(reader, 2, buffer)
+        chunks_filled = chunk_reader.read_chunks(reader, buffer_size, buffer)
         if chunks_filled == 1
             break
         end
 
-        results = decipher_blocks(buffer, block_cipher)
+        results = decipher_blocks(buffer[1:chunks_filled], block_cipher)
         chunk_writer.write_chunks(writer, results)
     end
     close(reader.input)
--- src/main.jl
+++ src/main.jl
@@ -23,79 +23,11 @@ using .chunk_reader
 include("aes_cipher.jl")
 using .aes_cipher
 
+BUFFER_SIZE = 10000000
+
 env_variables = config.read_config()
 cipher_key = aes_key.CipherKey((0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c))
-aes_cipher.cipher(env_variables["DECRYPTED_TEXT"], env_variables["ENCRYPTED_TEXT"], cipher_key)
+@time aes_cipher.cipher(env_variables["PLAIN_TEXT"], env_variables["ENCRYPTED_TEXT"], cipher_key, BUFFER_SIZE)
 
 #now try decipher
-aes_cipher.decipher(env_variables["ENCRYPTED_TEXT"], "data/dencrypted2.txt", cipher_key)
-
-
-
-
-# using Base.Threads
-
-# function thread_delay(a)
-#     sleep(1/a)
-#     a
-# end
-# function threaded_sqrt_array(A)
-#     B = similar(A)
-#     @threads for i in eachindex(A)
-#         @inbounds B[i] = thread_delay(A[i])
-#     end
-#     B
-# end
-# #make a vector from 1 to 10 in float 64
-# A::Vector{Float64} = 1:10
-# println(threaded_sqrt_array(A))
-# function sqrt_sum(A, chunk)
-#     s = zero(eltype(A))
-#     for i in chunk
-#         @inbounds s += sqrt(A[i])
-#     end
-#     return s
-# end
-
-# function threaded_sqrt_sum_workaround(A)
-#     chunks = Iterators.partition(eachindex(A), length(A) ÷ nthreads())
-#     tasks = map(chunks) do chunk
-#         @spawn sqrt_sum(A, chunk)
-#     end
-#     # s = mapreduce(fetch, +, tasks; init=zero(eltype(A)))
-#     s = map(fetch, tasks)
-#     return s
-# end
-# println(threaded_sqrt_sum_workaround(1:10))
-
-# my_tuple::NTuple{16, UInt8} = (0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c)
-# cipher_key = aes_key.CipherKey(my_tuple)
-# block_cipher = aes_block_cipher.new(cipher_key)
-# data_in::Vector{UInt8} = [0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34]
-# algo = aes_block_cipher.cipher_block(block_cipher.expanded_key, data_in)
-# for i in 1:16
-#     print(string(algo[i], base=16), " ")
-#     if i % 4 == 0
-#         println()
-#     end
-# end
-# for i in 1:10000000
-#     data_in = rand(UInt8, 16)
-#     data_out = aes_block_cipher.cipher_block(block_cipher.inv_expanded_key, data_in)
-# end
-
-# chunk = chunk_reader.ChunkReader(env_variables["DECRYPTED_TEXT"], Int(4 * N_B), true)
-# buffer::Vector{Vector{UInt8}} = [fill(UInt8(0), 16) for i in 1:2]
-# # @time chunk_reader.read_chunks(chunk, 2, buffer)
-# # println(buffer)
-# chunk_reader.read_chunks(chunk, 2, buffer)
-# # println(buffer)
-# writer = chunk_writer.ChunkWriter(env_variables["ENCRYPTED_TEXT"], true)
-
-# chunk_writer.write_chunks(writer, buffer)
-
-# println("finished")
-
-# Example usage:
-# env_variables = config.read_config()
-# println(env_variables["N_THREADS"])
+aes_cipher.decipher(env_variables["ENCRYPTED_TEXT"], env_variables["DECRYPTED_TEXT"], cipher_key, BUFFER_SIZE)
