26b43c3b7b1a39eab959bafc673874d94e9f98bd
ArmandoCivini <tcivini@gmail.com>
2024-04-11 15:54:06 -0300

    improved main readability

--- src/main.jl
+++ src/main.jl
@@ -32,11 +32,22 @@ using .StatsLogger
 
 function run_cipher()
     env_variables = config.read_config()
-    StatsLogger.initialize(env_variables["LOGGER_IP"], parse(Int, env_variables["LOGGER_PORT"]), env_variables["LOGGER_PREFIX"])
+    StatsLogger.initialize(env_variables["LOGGER_IP"],
+        parse(Int, env_variables["LOGGER_PORT"]), env_variables["LOGGER_PREFIX"]
+    )
     start = time()
 
-    cipher_key = aes_key.CipherKey((0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c))
-    cipher_stack = aes_cipher.new_cipher_stack(env_variables["PLAIN_TEXT"], env_variables["ENCRYPTED_TEXT"], env_variables["DECRYPTED_TEXT"], cipher_key, parse(Int, env_variables["BUFFER_SIZE"]), parse(Int, env_variables["N_THREADS"]), parse(Int, env_variables["TASK_PER_THREAD"]))
+    cipher_key = aes_key.CipherKey((0x2b, 0x7e, 0x15, 0x16,
+                                    0x28, 0xae, 0xd2, 0xa6,
+                                    0xab, 0xf7, 0x15, 0x88,
+                                    0x09, 0xcf, 0x4f, 0x3c)
+                                   )
+    cipher_stack = aes_cipher.new_cipher_stack(
+        env_variables["PLAIN_TEXT"], env_variables["ENCRYPTED_TEXT"],
+        env_variables["DECRYPTED_TEXT"], cipher_key,
+        parse(Int, env_variables["BUFFER_SIZE"]), parse(Int, env_variables["N_THREADS"]),
+        parse(Int, env_variables["TASK_PER_THREAD"])
+    )
     for i in 1:parse(Int, env_variables["REPEAT"])
         # VSCodeServer.@profview aes_cipher.cipher(cipher_stack)
         aes_cipher.cipher(cipher_stack)
